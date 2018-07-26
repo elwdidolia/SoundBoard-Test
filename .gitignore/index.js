4569cfef34a7317b3b26919b7accad2e9fe5f9f0
elwdidolia <41651782+elwdidolia@users.noreply.github.com>
2018-07-26 14:02:19 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,17 +1,20 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const YTDL = require("ytdl-core")
+ytdl(url, {
+  highWaterMark: 1024 * 1024 * 10 // 10 megabytes
+})
 
 const streamOptions = { seek : 0, volume : 1}
-const muse = YTDL('https://www.youtube.com/watch?v=GHxyGXqiJO4'{highWaterMark: 1024 * 1024 * 10});
-const mazette = YTDL('https://www.youtube.com/watch?v=3IUY_Pecku0'{filter : 'audioonly'}{
+const muse = YTDL('https://www.youtube.com/watch?v=GHxyGXqiJO4',{highWaterMark: 1024 * 1024 * 10});
+const mazette = YTDL('https://www.youtube.com/watch?v=3IUY_Pecku0', {filter : 'audioonly'}{
   highWaterMark: 1024 * 1024 * 10 // 10 megabytes
 });
 const BOTCMD = "!";
 ²
 bot.on('ready', function()
 {
-    console.log("Ca marche!");
+    console.log("Ca marche!");²
 });
 bot.on('message', message =>{
     switch(message.content)
